a4359d809ad48e69835dc8f0202a92de2a3b291a
evertonpsantos <evertonpsantos8@gmail.com>
2023-04-05 14:17:09 -0300

    feat: adds test to Product class

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    newProduct = Product(
+        1,
+        "Xablaunator",
+        "Xablau Inc",
+        "2023-12-13",
+        "2024-12-13",
+        "F8HU",
+        "a vácuo",
+    )
+
+    assert newProduct.nome_do_produto == 'Xablaunator'
+    assert newProduct.nome_da_empresa == 'Xablau Inc'
+    assert newProduct.data_de_fabricacao == "2023-12-13"
+    assert newProduct.data_de_validade == "2024-12-13"
+    assert newProduct.numero_de_serie == "F8HU"
+    assert newProduct.instrucoes_de_armazenamento == "a vácuo"
