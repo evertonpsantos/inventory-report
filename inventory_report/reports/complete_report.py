856232c85f356806965bcfef5d41c8ab2a8c9b9d
evertonpsantos <evertonpsantos8@gmail.com>
2023-04-05 17:59:58 -0300

    feat: adds method to CompleteReport

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,22 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(list):
+        companies_dict = {}
+        for product in list:
+            if product["nome_da_empresa"] not in companies_dict:
+                companies_dict[product["nome_da_empresa"]] = 0
+            companies_dict[product["nome_da_empresa"]] += 1
+
+        companies_str = ""
+        for company in companies_dict:
+            companies_str += f"- {company}: {companies_dict[company]}\n"
+
+        simple_report = SimpleReport.generate(list)
+        return (
+            f"{simple_report}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{companies_str}"
+        )
