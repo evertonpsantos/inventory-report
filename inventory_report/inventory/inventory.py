bc9e1fa6426245514a7c700be829ae4ac8fa1c39
evertonpsantos <evertonpsantos8@gmail.com>
2023-04-05 21:01:09 -0300

    feat: adds method to generate reports from json file

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -6,11 +7,19 @@ from inventory_report.reports.complete_report import CompleteReport
 class Inventory:
     @staticmethod
     def import_data(file_path, report_type):
-        with open(file_path, encoding="utf-8") as file:
-            products_csv = csv.DictReader(file, delimiter=",", quotechar='"')
-            product_data = [product for product in products_csv]
+        if file_path.endswith(".csv"):
+            with open(file_path, encoding="utf-8") as file:
+                products_csv = csv.DictReader(
+                    file, delimiter=",", quotechar='"'
+                )
+                product_data = [product for product in products_csv]
 
-        if (report_type == 'simples'):
+        if file_path.endswith(".json"):
+            with open(file_path, encoding="utf-8") as file:
+                products_json = json.load(file)
+                product_data = [product for product in products_json]
+
+        if report_type == "simples":
             return SimpleReport.generate(product_data)
         else:
             return CompleteReport.generate(product_data)
